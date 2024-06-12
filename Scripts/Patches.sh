eb9362e14745612bce94ff2635b7963c876c7c73
lietxia <7046884+lietxia@users.noreply.github.com>
2024-06-12 08:34:21 +0800

    Update Patches.sh

--- Scripts/Patches.sh
+++ Scripts/Patches.sh
@@ -5,3 +5,5 @@ if [[ $WRT_URL == *"immortalwrt"* && $WRT_TARGET == "Mediatek" ]]; then
 	cp -rf ./Patches/mediatek/*.dts ./wrt/target/linux/mediatek/dts/
 	patch -R -p1 -d ./wrt < ./Patches/mediatek/*.patch
 fi
+
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
